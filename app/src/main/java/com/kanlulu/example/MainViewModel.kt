f92f0ee2ff5a54be0a444d8d3ce314f67e031117
kanlulu <915948226@qq.com>
2021-08-15 22:19:13 +0800

    项目架构优化 base层与network组件层解耦

---
+++ app/src/main/java/com/kanlulu/example/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.kanlulu.example
+
+import com.iarc.base.viewmodel.BaseViewModel
+
+/**
+ * author:kanlulu
+ * data  :2021/8/15 10:10 下午
+ **/
+class MainViewModel : BaseViewModel() {
+
+}
